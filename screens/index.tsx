000bbb4c8d4e9de77c3f3b09cd8091c6fb1f5a75
mariaeduardasmidt <m.eduardasmidt@gmail.com>
2022-10-22 11:53:23 -0300

    Adicionando AsyncStorage na página de Watchlist.

--- screens/index.tsx
+++ screens/index.tsx
@@ -1,11 +1,61 @@
-import { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, FlatList, Alert } from "react-native";
+import { useState, useEffect } from "react";
+import { View, Text, TextInput, TouchableOpacity, FlatList, Alert, Keyboard } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Formik } from "formik";
+
 import { Movie } from "../components";
 import { styles } from './styles';
+import * as Yup from "yup";
 
-export function Home() {
+const saveMovie = async movieData => {
+  try {
+    const jsonValue = JSON.stringify(movieData);
+
+    await AsyncStorage.setItem("@movie", jsonValue);
+    return true;
+
+  } catch (err) {}
+  return false;
+};
+
+const getMovie = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem("@movie");
+
+    if (jsonValue !== null) {
+      const movieRecover = JSON.parse(jsonValue);
+
+      return movieRecover;
+    }
+  } catch (e) {}
+
+  return {
+    movie: ""
+  };
+};
+
+const MovieSchema = Yup.object().shape({
+  movie: Yup.string().min(2, "Mínimo de 2 caracteres.").required("Campo 'Filme' obrigatório."),
+});
+
+export function Home () {
   const [movies, setMovies] = useState<string[]>([])
   const [movieName, setMovieName] = useState('')
+  const [movieSaved, setMovieSaved] = useState(null);
+
+  async function restoreMovieSaved() {
+    const restoredMovie = await getMovie();
+    setMovieSaved(restoredMovie);
+  }
+
+  async function saveMovie(formData) {
+    Keyboard.dismiss();
+    const success = await saveMovie(formData);
+  }
+
+  useEffect(() => {
+    restoreMovieSaved();
+  }, []);
 
     function handleMovieAdd() {
       if (movies.includes(movieName)) {
@@ -34,45 +84,57 @@ export function Home() {
       }
 
     return (
-      <View style={styles.container}>
-        <Text style={styles.listTitle}>
-          Watchlist
-        </Text>
-        <Text style={styles.listSubtitle}>
-          Adicione filmes que deseja ver.
-        </Text>
-          <View style={styles.form}>
-            <TextInput
-              style={styles.input}
-              placeholder='Nome do filme'
-              placeholderTextColor='#fff'
-              onChangeText={setMovieName}
-              value={movieName}
-            />
-            <TouchableOpacity style={styles.button} onPress={handleMovieAdd}>
-              <Text style={styles.buttonText}>
-                +
-              </Text>
-            </TouchableOpacity>
-          </View>
+      <Formik
+        initialValues={{
+          nome: "",
+          email: "",
+        }}
+        validationSchema={MovieSchema}
+        onSubmit={async (values, actions) => {
+          await saveMovie(values);
+          actions.resetForm();
+        }}
+      >
+        <View style={styles.container}>
+          <Text style={styles.listTitle}>
+            Watchlist
+          </Text>
+          <Text style={styles.listSubtitle}>
+            Adicione filmes que deseja ver.
+          </Text>
+            <View style={styles.form}>
+              <TextInput
+                style={styles.input}
+                placeholder='Nome do filme'
+                placeholderTextColor='#fff'
+                onChangeText={setMovieName}
+                value={movieName}
+              />
+              <TouchableOpacity style={styles.button} onPress={handleMovieAdd}>
+                <Text style={styles.buttonText}>
+                  +
+                </Text>
+              </TouchableOpacity>
+            </View>
 
-        <FlatList
-          data={movies}
-          keyExtractor={item => item}
-          renderItem={({ item }) => (
-            <Movie
-              key={item}
-              name={item}
-              onRemove={() => handleMovieRemove(item)}
-            />
-          )}
-          showsHorizontalScrollIndicator={false}
-          ListEmptyComponent={() => (
-            <Text style={styles.listEmptyText}>
-              Watchlist vazia, adicione filmes desejados.
-            </Text>
-          )}
-        />
+          <FlatList
+            data={movies}
+            keyExtractor={item => item}
+            renderItem={({ item }) => (
+              <Movie
+                key={item}
+                name={item}
+                onRemove={() => handleMovieRemove(item)}
+              />
+            )}
+            showsHorizontalScrollIndicator={false}
+            ListEmptyComponent={() => (
+              <Text style={styles.listEmptyText}>
+                Watchlist vazia, adicione filmes desejados.
+              </Text>
+            )}
+          />
       </View>
-    )
-  }
+    </Formik>
+  )
+}
